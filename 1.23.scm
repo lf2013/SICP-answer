8f8ec9a5226b16a3e9de9c45c21dfbafe1c4a4a9
luoluo <luoluo1920@gmail.com>
2016-03-10 10:12:29 -0500

    c1.2

---
+++ 1.23.scm
@@ -0,0 +1,48 @@
+(define (smallest-divisor n)
+	(find-divisor 2 n))
+
+(define (find-divisor i n)
+	(cond ((> (* i i) n) n)
+		  ((= (remainder n i) 0) i)
+		  (else (find-divisor (next i) n))))
+
+(define (next i)
+    (if (= i 2) 3
+        (+ i 2)))
+
+(define (prime? n)
+	(= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+	(newline)
+	(display n)
+	(start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+	(if (prime? n)
+		(report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+	(display " **** ")
+	(display elapsed-time))
+
+(define (even? n)
+    (= (remainder n 2) 0))
+
+(define (search-for-prime l r)
+    (if (even? l)
+        (search-for-prime (+ l 1) r)
+    (cond ((< l r)
+        (timed-prime-test l)
+        (search-for-prime (+ l 2) r)))))
+
+(define (try)
+	(display (search-for-prime 1000 1020))
+	(display (search-for-prime 10000 10020))
+	(display (search-for-prime 100000 100020))
+	(display (search-for-prime 10000000 10000020))
+	(display (search-for-prime 100000000 100000020))
+	(display (search-for-prime 1000000000 1000000020))
+	(display (search-for-prime 10000000000 10000000020))
+	(display (search-for-prime 100000000000 100000000020))
+)
---
+++ 1.25.scm
@@ -0,0 +1,53 @@
+(define (fast-expt b e)
+    (cond ((= e 0) 1)
+          ((even? e) (square (fast-expt b (/ e 2))))
+          (else (* b (fast-expt b (- e 1) )))
+    )
+)
+
+(define (expmod base e m)
+    (remainder (fast-expt base e) m))
+
+(define (fast-test n)
+    (define (try-it a)
+    (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+    (cond ((= times 0) true)
+         ((fast-test n) (fast-prime? n (- times 1)))
+         (else false)))
+
+(define (timed-prime-test n)
+	(newline)
+	(display n)
+	(start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+	(if (fast-prime? n 10)
+		(report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+	(display " **** ")
+	(display elapsed-time))
+
+(define (even? n)
+    (= (remainder n 2) 0))
+
+(define (search-for-prime l r)
+    (if (even? l)
+        (search-for-prime (+ l 1) r)
+    (cond ((< l r)
+        (timed-prime-test l)
+        (search-for-prime (+ l 2) r)))))
+
+(define (try)
+	(display (search-for-prime 1000 1020))
+	(display (search-for-prime 10000 10020))
+	(display (search-for-prime 100000 100020))
+	(display (search-for-prime 10000000 10000020))
+	(display (search-for-prime 100000000 100000020))
+	(display (search-for-prime 1000000000 1000000020))
+	(display (search-for-prime 10000000000 10000000020))
+	(display (search-for-prime 100000000000 100000000020))
+)
