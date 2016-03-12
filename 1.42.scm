f4b22f4e57bd8e4901a176f4fc8516c06abb030d
luoluo <luoluo1920@gmail.com>
2016-03-12 06:26:20 -0500

    this one was a easy one

---
+++ 1.42.scm
@@ -0,0 +1,14 @@
+; 1.42
+(define (compose f g)
+    (lambda (x) (f (g x))))
+
+(define (square x)
+    (* x x))
+
+(define (inc x) (+ 1 x))
+
+(define (try)
+    ((compose square inc) 6))
+
+(define (try2)
+    ((compose inc square ) 6))
