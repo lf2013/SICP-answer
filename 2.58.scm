16826d649ee91ddfbbb4331da24417861d9bbd5b
luoluo <luoluo1920@gmail.com>
2016-03-24 08:59:33 -0400

    data abstraction means operations of a type regardless of the implementation detail.

---
+++ 2.58.scm
@@ -0,0 +1,49 @@
+; the thing worth doing typically take time and effort
+; 2.58
+
+(define (deriv expr var)
+    (cond ((number? expr) 0)
+          ((variable? expr)
+           (if (same-variable? expr var) 1 0))
+          ((sum? expr)
+            (make-sum
+                (deriv (addednd expr) var) (deriv (augend expr) var)))
+          ((product? expr)
+            (make-sum
+                (make-product (multiplier expr) (deriv (multiplicand expr) var))
+                (make-product (deriv (multiplier expr) var) (multiplicand expr))))
+          ((exponentiation? expr)
+            (let ((n (exponent expr)) (u (base expr)))
+                (cond ((= n 0) (deriv 1 var))
+                      ((= n 1) (deriv u var))
+                      (else (make-product (exponent expr)
+                                (make-product (make-exp (base expr) (- (exponent expr) 1))
+                                    (deriv (base expr) var)))))))
+          (else (error "unknown expression type -- DERIVE" expr))))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+        (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum v1 v2) (list v1 '+ v2))
+(define (sum? expr) (eq? '+ (cadr expr)))
+(define (addednd expr) (car expr))
+(define (augend expr) (caddr expr))
+
+(define (make-product v1 v2) (list v1 '* v2))
+(define (product? expr) (eq? '* (cadr expr)))
+(define (multiplier expr) (car expr))
+(define (multiplicand expr) (caddr expr))
+
+(define (make-exp v1 v2) (list v1 '** v2))
+(define (exponentiation? expr) (eq? '** (cadr expr)))
+(define (base expr) (car expr))
+(define (exponent expr) (caddr expr))
+
+(define (try)
+    (display (deriv '(x * y) 'x))
+    (newline)
+    (display (deriv '(x + 3) 'x))
+    ;(newline)
+    ;(display (deriv '(** x 3) 'x))
+)
