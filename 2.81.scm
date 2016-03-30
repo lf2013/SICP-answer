0380f0920b0280799462e1e15d8c93312cf3efdf
luoluo <luoluo1920@gmail.com>
2016-03-30 11:17:22 -0400

    update for same type

--- 2.81.scm
+++ 2.81.scm
@@ -4,7 +4,7 @@
 ; recursive for ever
 
 ; b
-; no, same type never need this.
+; no
 
 ; c
 
@@ -13,15 +13,17 @@
     (let ((type-tags (map type-tag args)))
         (let ((porc (get op type-tags)))
             (if proc
-                (apply proc (map contents args)) 
+                (apply proc (map contents args))
                 (if (= 2 (length args))
                     (let ((type1 (type-tag (car args)))
                           (type2 (type-tag (cadr args))))
+                    (if (eq? type1 type2)
+                        (error "not defined operation for type1")
                     (let ((t1->t2 (get-coercion type1 type2)))
                         (if t1->t2
                              (apply-generic op (t1->t2 t1) t2)
                              (let ((t2->t1 (get-coercion type2 type1)))
                                    (if t2->t1
                                          (apply-generic op t1 (t2->t1 t2))
-                                         (error "no such operation"))))))
+                                         (error "no such operation")))))))
                               (error "no such operation"))))))
