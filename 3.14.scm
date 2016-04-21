811eb8b8a8e3c890d8e51979c642d152cf968b35
luoluo <luoluo1920@gmail.com>
2016-04-21 10:23:30 -0400

    not so normal

---
+++ 3.14.scm
@@ -0,0 +1,17 @@
+; things worth doing typically take time and effort
+; 3.14 reverse work
+
+(define (mystery x)
+    (define (loop x y)
+        (if (null? x)
+            y
+        (let ((temp (cdr x)))
+            (set-cdr! x y)
+            (loop temp x))))
+    (loop x '()))
+
+(define (try)
+    (define a (list 'a 'b 'c 'd))
+    (display a)
+    (newline)
+    (display (mystery a)))
