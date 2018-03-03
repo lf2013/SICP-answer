a46eb98e0699843a87998769a1c68ea554b880ad
Luo <luoluo1920@gmail.com>
2018-03-03 11:49:01 +0800

    answer 3.40

--- 3.40.scm
+++ 3.40.scm
@@ -15,3 +15,12 @@
 	 (lambda () (set! x (* x x)))
 	 (lambda () (set! x (* x x x))))
 )
+
+; possible result:
+; (10 * 10) * (10 * 10) * (10 * 10)
+; (10 * 10 * 10) * (10 * 10 * 10)
+(define x 10)
+(define s (make-serializer))
+(parallel-execute 
+ (s (lambda () (set! x (* x x))))
+ (s (lambda () (set! x (* x x x)))))
