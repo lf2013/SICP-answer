45c670b9f48c50a7456503c753b93125774d19ea
Luo <luoluo1920@gmail.com>
2018-03-03 12:18:36 +0800

    answer 3.42

---
+++ 3.42.scm
@@ -0,0 +1,30 @@
+; things worth doing typically take time and effort
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance 
+                     (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw 
+           (protected withdraw))
+          (protected-deposit 
+           (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) 
+               protected-withdraw)
+              ((eq? m 'deposit) 
+               protected-deposit)
+              ((eq? m 'balance) 
+               balance)
+              (else 
+               (error "Unknown request: 
+                       MAKE-ACCOUNT"
+                      m))))
+      dispatch)))
+; safe, but maybe a little bit slower by adding a wrapper
