78bae0a08b401d727a590178d2e50d8e70e78c4e
Luo <luoluo1920@gmail.com>
2018-03-06 13:28:26 +0800

    answer 3.60

---
+++ 3.60.scm
@@ -0,0 +1,125 @@
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low 
+                   (stream-enumerate-interval (+ 1 low) high))))
+(define (stream-ref s n)
+  (if (= n 0) (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams )
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+		; (begin (display 'x)
+        (apply proc (map stream-car argstreams))
+		; )
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) 
+         the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream 
+          (stream-car stream)
+          (stream-filter 
+           pred
+           (stream-cdr stream))))
+        (else (stream-filter 
+               pred 
+               (stream-cdr stream)))))
+
+(define (show x)
+  (display x)(newline)
+    x)
+
+(define (display-line x)
+	(newline) (display x))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin 
+        (proc (stream-car s))
+        (stream-for-each proc 
+                         (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (scale-stream stream factor)
+  (stream-map
+   (lambda (x) (* x factor))
+   stream))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream 
+                   s1car 
+                   (merge (stream-cdr s1) 
+                          s2)))
+                 ((> s1car s2car)
+                  (cons-stream 
+                   s2car 
+                   (merge s1 
+                          (stream-cdr s2))))
+                 (else
+                  (cons-stream 
+                   s1car
+                   (merge 
+                    (stream-cdr s1)
+                    (stream-cdr s2)))))))))
+
+(define (expand num den radix)
+  (cons-stream
+   (quotient (* num radix) den)
+   (expand (remainder (* num radix) den) 
+           den 
+           radix)))
+
+(define (add-streams s1 s2)
+	(stream-map + s1 s2))
+
+(define (mul-stream s1 s2)
+	(stream-map * s1 s2))
+
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+				(add-streams (scale-stream (stream-cdr s1) (stream-car s2)) 
+							 (scale-stream (stream-cdr s2) (stream-car s1)))))
+
+(define (int n) (cons-stream n (int (+ 1 n))))
+
+(define (integrate-series s) (stream-map / s (int 1)))
+
+(define exp-series
+  (cons-stream 
+   1 (integrate-series exp-series)))
+
+(define cosine-series 
+  (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series  cosine-series)))
+
+(define exp-series
+  (cons-stream 
+   1 (integrate-series exp-series)))
+
+(define (try)
+	(stream-head exp-series 5)
+	(display (stream-head sine-series 10)) (newline)
+	(display (stream-head cosine-series 10)) (newline)
+	(display (stream-head (add-streams sine-series cosine-series) 10)) (newline)
+	(display (stream-head (mul-stream sine-series cosine-series) 10)) (newline)
+	(display (stream-head (mul-series sine-series cosine-series) 10)) (newline)
+)
+
+(try)
